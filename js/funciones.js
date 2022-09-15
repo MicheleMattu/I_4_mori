d6d45e2f7c9a36633a2f12f885e098f717b856b4
Michele Mattu <michelemattu26@alumnos.cei.es>
2022-09-15 21:16:47 +0200

    empezado otra vez el responsive empezando por los headers de todas las paginas

--- js/funciones.js
+++ js/funciones.js
@@ -1,1 +1,15 @@
-console.log("conectado");
+const navegacion = document.querySelector("nav");
+const botones = document.querySelectorAll(".abrir,.cerrar");
+const input = document.querySelector("input[type=submit");
+function toggleNavegacion(evento){
+	evento.preventDefault();
+	navegacion.classList.toggle("desplegado");
+}
+
+botones.forEach(function(boton){
+	boton.addEventListener("click", toggleNavegacion);
+});
+	
+input.addEventListener("submit", function(){
+	input.preventDefault();
+});
